8833986e5bda5e805f26a275055a863722fe4f48
Alex Dzehil <dirolex33@mail.ru>
2021-09-28 21:55:16 +0300

    Photo in menu

--- bar/templatetags/get_global_counter.py
+++ bar/templatetags/get_global_counter.py
@@ -1,12 +1,25 @@
 from django import template
 
+from bar.models import ImageMenu, SubMenu
+
 register = template.Library()
 
+i = 0
 
 @register.simple_tag
-def get_global_counter(inner, outer, items):
-    print(inner)
-    print(outer)
-    print(items)
+def get_global_counter(inner, outer, items, im_cou):
+    count = ImageMenu.objects.all().count()
+    # print('Всего фото', count)
+    # print('Фото в подменю', im_cou)
+    # print('Итерация внутреннего цикла', inner)
+    # print('Итерация внешнего цикла', outer)
+    # print(items)
+    # print('Искомый айдишник', count - (inner + outer))
+    global i
+    if i <= count:
+        i += 1
+        print('Искомое число', i)
+    else:
+        print('Все')
     print('='*80)
-    return int(inner)
+    return i
